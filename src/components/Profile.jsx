ab13048fd2cd127cafadf2a821a992950d69a6d7
bestchayapol <best37@hotmail.com>
2023-03-25 19:02:16 +0700

    First Commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile() {
+    return (
+        <div className="layout">
+            <img src="assets/myProfile.jpeg" alt="myProfile" />
+            <h1 className="name">Best Chayapol</h1>
+            <hr />
+        </div>
+    )
+}
+export default Profile;
