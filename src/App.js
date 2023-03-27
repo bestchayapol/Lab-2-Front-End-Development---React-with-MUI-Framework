2d1f8014e05ab613d41eb1b18290c612ecd44f78
bestchayapol <best37@hotmail.com>
2023-03-27 14:47:04 +0700

    Second Commit -Transform web application that support Responsive Design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import './App.css';
-import Home from './components/Home';
-import Todo from './components/Todo';
+import TodoPage from './pages/TodoPage';
 
 function App() {
   return (
-    <div className="App">
-      <div className='home'>
-        <Home />
-      </div>
-      <div className='todo'>
-        <Todo />
-      </div>
-    </div>
+    <TodoPage />
   );
 }
 
